a5da8a1903fa436f29f97005c9dc14489ef1c7d3
Satyajit Bihari <biharisatyajit@gmail.com>
2018-04-26 13:01:15 +0530

    Adding demo jenkins project

---
+++ src/Data/Demo1.java
@@ -0,0 +1,19 @@
+package Data;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+    @Test
+	
+	public void test() {
+	System.out.println("Hello world");
+	}
+	
+    
+    @Test
+    
+    public void test1() {
+    	System.out.println("Hello Bubun");
+    }
+}
